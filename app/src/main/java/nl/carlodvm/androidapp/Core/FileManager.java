28dfe194afc798d2ad40cebb1d33b52b0ccded52
MrCtje <c.daveiga@live.nl>
2018-11-26 18:02:22 +0100

    Change streams for file manipulation

--- app/src/main/java/nl/carlodvm/androidapp/Core/FileManager.java
+++ app/src/main/java/nl/carlodvm/androidapp/Core/FileManager.java
@@ -3,10 +3,14 @@ package nl.carlodvm.androidapp.Core;
 import android.content.Context;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class FileManager {
     private Context context;
@@ -24,24 +28,26 @@ public class FileManager {
         return createFile(filename).exists();
     }
 
-    public FileInputStream getFileInputStream(String filename){
+    public BufferedReader getFileInputStreamReader(String filename){
         FileInputStream fis = null;
         try {
             fis = context.openFileInput(filename);
         } catch (FileNotFoundException e) {
             Log.e(FileManager.class.getSimpleName(), "File does not exist.");
         }
-        return fis;
+        BufferedInputStream ir = new BufferedInputStream(fis);
+        BufferedReader br = new BufferedReader(new InputStreamReader(ir));
+        return br;
     }
 
-    public FileOutputStream getFileOutputStream(String filename){
+    public OutputStreamWriter getFileOutputStream(String filename){
         FileOutputStream fos = null;
         try {
             fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
         } catch (FileNotFoundException e) {
             Log.e(FileManager.class.getSimpleName(), "File does not exist.");
         }
-        return fos;
+        return new OutputStreamWriter(fos);
     }
 
 }
